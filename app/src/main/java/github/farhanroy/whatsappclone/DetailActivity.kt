6247bb9d6b87b615637934f0b2dc930989b510cc
farhanroy <farhanroy120@gmail.com>
2021-06-28 07:25:31 +0700

    :hammer: built text message box

--- app/src/main/java/github/farhanroy/whatsappclone/DetailActivity.kt
+++ app/src/main/java/github/farhanroy/whatsappclone/DetailActivity.kt
@@ -7,21 +7,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -175,4 +177,25 @@ fun PeerBubble(message: Message) {
 
 @Composable
 fun MessageBox() {
+
+    val textState = remember { mutableStateOf(TextFieldValue()) }
+    val scrollState = rememberScrollState()
+
+    Box(Modifier.background(MaterialTheme.colors.primary)) {
+        Row(modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()) {
+            BasicTextField(
+                value = textState.value,
+                modifier = Modifier.weight(1f, true),
+                onValueChange = {
+                    textState.value = it
+                })
+            Spacer(modifier = Modifier.size(12.dp))
+
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Send, contentDescription = null)
+            }
+        }
+    }
 }
